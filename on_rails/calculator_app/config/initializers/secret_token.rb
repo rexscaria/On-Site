075bbf12ce13f91d70fca1ac2e8273a43b63f225
Scaria <arunscaria91@gmail.com>
2016-04-05 15:43:29 -0700

    Skeleton for rails app

---
+++ on_rails/calculator_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CalculatorApp::Application.config.secret_key_base = '7d54fda75a7e733f6c97096baa2e8301696215d9b76dec09d00a607edb16b3ae29ec5976be3b5194eee87af472a012748e73c929d0a6cac17dd0c01750705a0d'
