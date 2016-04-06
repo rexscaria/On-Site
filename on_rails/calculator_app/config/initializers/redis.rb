746404db7ba44412d040305e9c6ee205e862e719
Scaria <arunscaria91@gmail.com>
2016-04-06 02:20:15 -0700

    Production debug changed

--- on_rails/calculator_app/config/initializers/redis.rb
+++ on_rails/calculator_app/config/initializers/redis.rb
@@ -2,7 +2,7 @@
 redis_host = ENV["REDISTOGO_URL"] || '127.0.0.1:6379'
 uri = URI.parse(redis_host)
 DataMapper.setup(:default, {:adapter  => "redis"})
-Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Redis.current = Redis.new(:uri => uri)
 
 
 #$redis = Redis::Namespace.new("calculator_app", :redis => Redis.new)
