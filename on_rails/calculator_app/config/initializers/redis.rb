05ac5b252c517271860a4bcb0a7a10389ea343eb
Scaria <arunscaria91@gmail.com>
2016-04-06 02:16:56 -0700

    Redis port changed

--- on_rails/calculator_app/config/initializers/redis.rb
+++ on_rails/calculator_app/config/initializers/redis.rb
@@ -1,8 +1,8 @@
 
-redis_host = ENV["REDISTOGO_URL"] || '127.0.0.1'
+redis_host = ENV["REDISTOGO_URL"] || '127.0.0.1:6379'
 uri = URI.parse(redis_host)
 DataMapper.setup(:default, {:adapter  => "redis"})
-Redis.current = Redis.new(:host => uri.host, :port => 6379)
+Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 
 #$redis = Redis::Namespace.new("calculator_app", :redis => Redis.new)
